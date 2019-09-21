4934c9ea08d0305409287ec7ffa3ab9d4bd52c8a
OhYee <oyohyee@oyohyee.com>
2019-09-21 23:00:43 +0800

    add: makefile template

---
+++ utils/make_utils.mk
@@ -0,0 +1,88 @@
+############################################
+#         Makefile for c projects          #
+#              oyohyee@oyohyee.com         #
+############################################
+#
+# Make a file in source folder, and input:
+# 
+# PROGRAM := <Output file name>
+# ROOT := <Root of the file>
+# EXTRA_DIRS := <Other source folder>
+# include <Path to this file>
+#
+
+ifeq ($(ROOT),)
+	ROOT = ./
+endif
+
+RM = rm -f
+PS = c
+CC = gcc
+CFLAGS = -g -Wall --std=c99
+
+MAIN_FILE_PATH = $(ROOT)$(MAIN_FILE)
+PROGRAM = $(MAIN_FILE_PATH).out
+
+PWD = `pwd`
+SUBDIRS = $(foreach d,$(EXTRA_DIRS),$(shell ls $(d) -F | grep "\/" | sed "s:^:$(d):" | grep -v "test")) 
+DIRS = $(SUBDIRS) $(EXTRA_DIRS)
+
+SOURCE = $(foreach d,$(DIRS),$(wildcard $(d)*.$(PS)))
+ifneq ($(MAIN_FILE),)
+	SOURCE += $(MAIN_FILE_PATH).$(PS)
+endif
+OBJS:=$(patsubst %.$(PS),%.o,$(SOURCE))
+DEPS:=$(patsubst %.o,%.d,$(OBJS))
+MISSING_DEPS:=$(filter-out $(wildcard $(DEPS)),$(DEPS))
+MISSING_DEPS_SOURCE:=$(wildcard $(patsubst %.d,%.$(PS),$(MISS_DEPS)))
+
+
+ifeq ($(MAIN_FILE),)
+
+.PHONY:all clean info test
+all: 
+	-@$(MAKE) -s test && $(MAKE) -s clean
+test:
+	-@$(MAKE) -C ./test -s run;
+
+else
+
+.PHONY:all clean info build run
+all:
+	-@$(MAKE) -s run && $(MAKE) -s clean
+build: $(PROGRAM)
+run: build
+	@./$(PROGRAM)
+$(PROGRAM):$(SOURCE:.$(PS)=.o)
+	@$(CC) $(CFLAGS) -o "$(PROGRAM)" $(OBJS)
+%.o:%.c
+	@$(CC) $(CFLAGS) -c -o "$@" "$<"
+%.d:%.c
+	@$(CC) $< -MM -MP -MT "$*.o" -MF "$*.d"
+-include $(DEPS)
+
+endif
+
+clean:
+	-@$(RM) $(DEPS)
+	-@$(RM) $(OBJS)
+	-@$(RM) $(PROGRAM)
+
+info:
+	@echo "CC:                       $(CC)"
+	@echo "CFLAGS:                   $(CFLAGS)"
+	@echo "RM:                       $(RM)"
+	@echo ""
+	@echo "root:                     $(ROOT)"
+	@echo "pwd:                      $(PWD)"
+	@echo "EXTRA_DIRS:               $(EXTRA_DIRS)"
+	@echo "SUBDIRS:                  $(SUBDIRS)"
+	@echo "DIRS:                     $(DIRS)"
+	@echo ""
+	@echo "MAIN_FILE:                $(MAIN_FILE)"
+	@echo "PROGRAM:                  $(PROGRAM)"
+	@echo "SOURCE:                   $(SOURCE)"
+	@echo "OBJS:                     $(OBJS)"
+	@echo "DEPS:                     $(DEPS) "
+	@echo "MISSING_DEPS:             $(MISSING_DEPS)"
+	@echo "MISSING_DEPS_SOURCE: 	 $(MISSING_DEPS_SOURCE)"
