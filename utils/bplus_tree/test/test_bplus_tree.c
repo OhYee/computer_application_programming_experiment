288ede575c113a5b8769330df4ec1bae9973c1e1
OhYee <oyohyee@oyohyee.com>
2019-10-21 23:45:58 +0800

    test: b+ tree test file(#master)

--- utils/bplus_tree/test/test_bplus_tree.c
+++ utils/bplus_tree/test/test_bplus_tree.c
@@ -23,18 +23,45 @@ int main() {
     mp_init(1 << 20, mp_exit);
     a = mp_new(sizeof(int));
     bplus_tree *bpt = bpt_init(3, compare);
-    bpt_add(bpt, num(19), sizeof(int));
-    bpt_add(bpt, num(18), sizeof(int));
-    bpt_add(bpt, num(17), sizeof(int));
+    bpt_add(bpt, num(20 - 1), sizeof(int));
+    bpt_add(bpt, num(20 - 2), sizeof(int));
+    bpt_add(bpt, num(20 - 3), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(16), sizeof(int));
+    bpt_add(bpt, num(20 - 4), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(15), sizeof(int));
+    bpt_add(bpt, num(20 - 5), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(14), sizeof(int));
+    bpt_add(bpt, num(20 - 6), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 7), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 8), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 9), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 10), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 11), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 18), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 19), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 12), sizeof(int));
+    bpt_print(bpt);
+
+    bpt_add(bpt, num(20 - 13), sizeof(int));
     bpt_print(bpt);
 
     return 0;
