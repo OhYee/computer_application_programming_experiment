499f7fb245b90b472cfb54c44a723e4f0355f989
OhYee <oyohyee@oyohyee.com>
2019-10-22 16:13:40 +0800

    test: B+ tree test file(#master)

--- utils/bplus_tree/test/test_bplus_tree.c
+++ utils/bplus_tree/test/test_bplus_tree.c
@@ -1,3 +1,4 @@
+#include "../../print/print.h"
 #include "../bplus_tree.h"
 #include <memory.h>
 
@@ -23,46 +24,78 @@ int main() {
     mp_init(1 << 20, mp_exit);
     a = mp_new(sizeof(int));
     bplus_tree *bpt = bpt_init(3, compare);
-    bpt_add(bpt, num(20 - 1), sizeof(int));
-    bpt_add(bpt, num(20 - 2), sizeof(int));
-    bpt_add(bpt, num(20 - 3), sizeof(int));
+    bpt_add(bpt, num(1), sizeof(int));
+    bpt_add(bpt, num(1), sizeof(int));
+    bpt_add(bpt, num(2), sizeof(int));
+    bpt_add(bpt, num(3), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 4), sizeof(int));
+    bpt_add(bpt, num(4), sizeof(int));
+    bpt_add(bpt, num(4), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 5), sizeof(int));
+    bpt_add(bpt, num(5), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 6), sizeof(int));
+    bpt_add(bpt, num(6), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 7), sizeof(int));
+    bpt_add(bpt, num(7), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 8), sizeof(int));
+    bpt_add(bpt, num(8), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 9), sizeof(int));
+    bpt_add(bpt, num(9), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 10), sizeof(int));
+    bpt_add(bpt, num(10), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 11), sizeof(int));
+    bpt_add(bpt, num(11), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 18), sizeof(int));
+    bpt_add(bpt, num(18), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 19), sizeof(int));
+    bpt_add(bpt, num(19), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 12), sizeof(int));
+    bpt_add(bpt, num(12), sizeof(int));
     bpt_print(bpt);
 
-    bpt_add(bpt, num(20 - 13), sizeof(int));
+    bpt_add(bpt, num(13), sizeof(int));
     bpt_print(bpt);
+
+    boolean pass = T;
+    int     testcases[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 18, 19};
+    for (int i = 0; i < sizeof(testcases) / 8; ++i) {
+        int n = testcases[i];
+        if (bpt_exist(bpt, num(n)) == T) {
+            print_ok("Search %d passed.\n", n);
+        } else {
+            print_err("Search %d has error!\n", n);
+            pass = F;
+        }
+    }
+    if (bpt_exist(bpt, num(0)) == F) {
+        print_ok("Search %d passed.\n", 0);
+    } else {
+        print_err("Search %d has error!\n", 0);
+        pass = F;
+    }
+    if (bpt_exist(bpt, num(23)) == F) {
+        print_ok("Search %d passed.\n", 23);
+    } else {
+        print_err("Search %d has error!\n", 23);
+        pass = F;
+    }
+
+    if (pass) {
+        print_ok("All testcases passed.\n");
+    } else {
+        print_err("Has error!\n");
+    }
 
     return 0;
 }
