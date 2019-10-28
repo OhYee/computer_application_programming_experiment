184a8d44c9e10dcab4d44fc4faae4ad61085141e
OhYee <oyohyee@oyohyee.com>
2019-10-28 23:46:18 +0800

    misc: header file of radix tree(#master)

--- utils/radix_tree/radix_tree.h
+++ utils/radix_tree/radix_tree.h
@@ -1,3 +1,4 @@
+#include "../bits/bits.h"
 #include "../boolean/boolean.h"
 #include "../free/free.h"
 #include <memory.h>
@@ -7,12 +8,22 @@ typedef struct _radix_tree_node radix_tree_node;
 
 struct _radix_tree {
     radix_tree_node *root;
+    int              k;
+    bits *           temp;
 };
+
+radix_tree *rt_init(int k);
+void        rt_add(radix_tree *rt, void *value, int size);
+void        rt_print(radix_tree *rt);
 
 struct _radix_tree_node {
     radix_tree_node **children;
-    void *            value;
-    int               length;
+    boolean           end_of_value; // is this node end of the value
+    bits *            value;
 };
 
-int get_prefix(void *a, int a_l, void *b, int b_l, int k);
+radix_tree_node *rtn_init(int m);
+radix_tree_node *rtn_add(radix_tree_node *rtn, bits *value, int k,
+                         boolean memory);
+void             rtn_print(radix_tree_node *rtn, int m);
+int              pow2(int m);
